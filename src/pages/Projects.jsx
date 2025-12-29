8ae366b89ce9f35560a177f79ff764296fc04f8a
seif-el-sayed1 <seifsayed121@gmail.com>
2025-12-29 18:47:17 +0200

    Add icons to contribution and in-progress indicators, and update link text for clarity

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -26,13 +26,13 @@ export const Projects = () => {
 
                     {project.contribution && (
                         <div className="absolute top-2 left-2 bg-green-500 text-white px-3 py-1 rounded-lg text-xs font-bold shadow-md z-20">
-                            Contribution
+                            🚀 Contribution
                         </div>
                     )}
 
                     {project.inProgress && (
                         <div className="absolute top-12 left-2 bg-yellow-500 text-white px-3 py-1 rounded-lg text-xs font-bold shadow-md z-20">
-                            In Progress
+                            ⌛ In Progress
                         </div>
                     )}
 
@@ -77,7 +77,7 @@ export const Projects = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
-                                {project.projectPrivate ? <span>Private</span> : <a href={project.link} target='_blank' rel='noreferrer'>Website</a>}
+                                {project.projectPrivate ? <span>Private</span> : <a href={project.link} target='_blank' rel='noreferrer'>View</a>}
                             </div>
                         )}
 
