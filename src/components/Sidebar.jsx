25aa85c611e6d9d0da3ea900f90bd95901adb0d1
seif-el-sayed1 <seifsayed121@gmail.com>
2025-12-29 02:25:39 +0200

    Update Sidebar component height for improved layout consistency

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export const Sidebar = () => {
     const {sideBar} = useContext(AppContext)
 
     return (
-        <div className={`ml-5 h-screen rounded-lg px-3  md:ml-15 absolute md:hidden 
+        <div className={`ml-5 h-130 rounded-lg px-3  md:ml-15 absolute md:hidden 
                 border-1 border-white duration-500 ${sideBar ? "left-0" :  " -left-30" }`}>
             <ul className='flex flex-col items-start gap-4 text-white text-lg mt-3'>
                 {sideLinks.map((link, index) => {
