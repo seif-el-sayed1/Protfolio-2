a2911c7a9b4d657242fa553670295c0c3c5daede
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-20 00:12:23 +0200

    UPDATE  CV link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ export const Home = () => {
                 `}>
                 <div 
                     className='flex items-center gap-2 text-lg sm:text-xl px-5 py-2 border-1 border-[var(--secondary-color)] hover:bg-[var(--secondary-color)] rounded-2xl cursor-pointer duration-200'
-                    onClick={() => window.open("https://drive.google.com/file/d/17wD-euySHL_0rnGCVExkhoakzuInO0GU/view?usp=drive_link", "_blank")}
+                    onClick={() => window.open("https://drive.google.com/file/d/17oBOj-waB5o-qm8SGboByjj7caJqVzxk/view?usp=drive_link", "_blank")}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
